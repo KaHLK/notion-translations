d31f7ae5463c6a9603ad9eee1b8f281b2db7adc9
Kristoffer Kabat <yeah-kris@hotmail.com>
2023-07-01 21:47:45 +0200

    Remove unused types

--- src/util/cli.ts
+++ src/util/cli.ts
@@ -1,15 +1,5 @@
 import prompts from "prompts";
 
-import { enumerate, range } from "./fn";
-import { Option, none, some } from "./option";
-
-type PrintFn<T> = (v: T, idx: number) => string;
-type ParseFn = (
-    answer: string,
-    min: number,
-    max: number,
-) => Option<Set<number>>;
-
 export async function select_from_list<T>(
     msg: string,
     list: { title: string; value: T }[],
