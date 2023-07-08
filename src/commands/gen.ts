48645d0c092812d60ba747baaa69480a2d5a6b0f
Kristoffer Kabat <yeah-kris@hotmail.com>
2023-07-08 15:18:41 +0200

    Strongly type the strings and move the page parsing into it's own function

--- src/commands/gen.ts
+++ src/commands/gen.ts
@@ -9,6 +9,11 @@ type Properties = PageObjectResponse["properties"][string];
 type Title = Extract<Properties, { type: "title" }>;
 type RichText = Extract<Properties, { type: "rich_text" }>;
 
+type Lng = string & { readonly lng: unique symbol };
+type Key = string & { readonly title: unique symbol };
+
+type Language = Map<Key, { value: RichText; context: RichText }>;
+
 interface GenerateOptions {
     format: "i18next" | "android";
     category?: boolean;
@@ -33,52 +38,14 @@ export async function generate(
     }
 
     console.log("Parsing pages");
-    // TODO: Turn duplicates into map
-    const duplicates: [string, RichText][] = [];
-    const missing_translations: Map<string, string[]> = new Map();
-    type Language = Map<string, { value: RichText; context: RichText }>;
-    const map: Map<string, Language> = new Map();
-    for (const page of pages) {
-        const properties = Object.entries(page.properties);
-        const _title = properties.find(([_, p]) => p.type === "title");
-        const _context = properties.find(([n]) => n === "context");
-        const rest = properties.filter(
-            ([n, p]) => n !== "context" && p.type === "rich_text",
-        ) as [string, RichText][];
+    const { duplicates, missing, languages } = parse_pages(pages);
 
-        if (!_title || !_context) {
-            console.log(
-                `Page with id '${page.id}' did not contain a title or context column`,
-            );
-            continue;
-        }
-        const title = (_title[1] as Title).title
-            .map((t) => t.plain_text)
-            .join("_");
-        const context = _context[1] as RichText;
-
-        for (const [name, prop] of rest) {
-            if (prop.rich_text.length === 0) {
-                const arr = missing_translations.get(name) ?? [];
-                missing_translations.set(name, arr.concat(title));
-                continue;
-            }
-
-            const dict: Language = map.get(name) ?? new Map();
-            if (dict.has(name)) {
-                duplicates.push([title, prop]);
-                continue;
-            }
-            dict.set(title, {
-                value: prop,
-                context,
-            });
-            map.set(name, dict);
-        }
-    }
-
-    if (!options.ignore && duplicates.length > 0) {
-        console.log("Found", duplicates.length, "duplicates");
+    const num_duplication = Array.from(duplicates.values()).reduce(
+        (acc, cur) => acc + cur.length,
+        0,
+    );
+    if (!options.ignore && num_duplication > 0) {
+        console.log("Found", num_duplication, "duplicates");
         for (const [title, prop] of duplicates) {
             notImplementedYet(
                 "TODO: Handle duplicate keys. If option.ci === true, say no to all prompts (just list the duplicates)",
@@ -86,7 +53,7 @@ export async function generate(
         }
     }
 
-    const num_missing = Array.from(missing_translations.values()).reduce(
+    const num_missing = Array.from(missing.values()).reduce(
         (acc, cur) => acc + cur.length,
         0,
     );
@@ -101,7 +68,7 @@ export async function generate(
             ).expect("Please answer with a 'y' or 'n'.");
         }
         if (answer) {
-            for (const [lang, keys] of missing_translations.entries()) {
+            for (const [lang, keys] of missing.entries()) {
                 console.group(`${lang}:`);
                 for (const key of keys) {
                     console.log(`'${key}'`);
@@ -113,4 +80,61 @@ export async function generate(
     }
 
     notImplementedYet("TODO: Generate the files");
+}
+
+function parse_pages(pages: PageObjectResponse[]): {
+    duplicates: Map<string, RichText[]>;
+    missing: Map<string, string[]>;
+    languages: Map<Lng, Language>;
+} {
+    const missing: Map<Lng, Key[]> = new Map();
+    const duplicates: Map<Key, RichText[]> = new Map();
+
+    const languages: Map<Lng, Language> = new Map();
+    for (const page of pages) {
+        const properties = Object.entries(page.properties);
+        const _title = properties.find(([_, p]) => p.type === "title");
+        const _context = properties.find(([n]) => n === "context");
+
+        if (!_title || !_context) {
+            console.log(
+                `Page with id '${page.id}' did not contain a title or context column`,
+            );
+            continue;
+        }
+
+        const title = (_title[1] as Title).title
+            .map((t) => t.plain_text)
+            .join("_") as Key;
+        const context = _context[1] as RichText;
+        const rest = properties.filter(
+            ([n, p]) => n !== "context" && p.type === "rich_text",
+        ) as [Lng, RichText][];
+
+        for (const [name, prop] of rest) {
+            if (prop.rich_text.length === 0) {
+                const arr = missing.get(name) ?? [];
+                missing.set(name, arr.concat(title));
+                continue;
+            }
+
+            const dict: Language = languages.get(name) ?? new Map();
+            if (dict.has(title)) {
+                const arr = duplicates.get(title) ?? [];
+                duplicates.set(title, arr.concat(prop));
+                continue;
+            }
+            dict.set(title, {
+                value: prop,
+                context,
+            });
+            languages.set(name, dict);
+        }
+    }
+
+    return {
+        duplicates,
+        missing,
+        languages,
+    };
 }
