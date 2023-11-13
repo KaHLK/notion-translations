c340de76791337fe02d9a8c096ee4f7be4099dc1
Kristoffer Kabat <yeah-kris@hotmail.com>
2023-11-13 17:45:56 +0100

    Add a delete function to the `GenCache`

--- src/util/fs.ts
+++ src/util/fs.ts
@@ -1,5 +1,8 @@
 import { constants } from "fs";
-import { access, writeFile } from "fs/promises";
+import { access, writeFile, rm, rmdir, readdir, stat } from "fs/promises";
+import { join } from "path";
+
+import { Result, err, ok } from "./result";
 
 export async function exists(path: string): Promise<boolean> {
     try {
@@ -12,4 +15,31 @@ export async function exists(path: string): Promise<boolean> {
 
 export async function save(path: string, contents: string) {
     await writeFile(path, contents, { encoding: "utf8" });
+}
+
+export async function delete_dir(path: string): Promise<Result<null, Error>> {
+    const entries = await readdir(path);
+    for (const entry of entries) {
+        const p = join(path, entry);
+        const stats = await stat(p);
+        if (stats.isDirectory()) {
+            const res = await delete_dir(p);
+            if (res.isErr()) {
+                return res;
+            }
+            continue;
+        }
+        try {
+            await rm(p);
+        } catch (e) {
+            return err(e as Error);
+        }
+    }
+    try {
+        await rmdir(path);
+    } catch (e) {
+        return err(e as Error);
+    }
+
+    return ok(null);
 }
